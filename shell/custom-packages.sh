ed4bdfc23d92c1059341db1b702862ccde54d378
adai6670 <61269317+adai6670@users.noreply.github.com>
2026-02-17 12:50:44 +0800

    Update custom-packages.sh

--- shell/custom-packages.sh
+++ shell/custom-packages.sh
@@ -28,6 +28,10 @@ CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-homeproxy"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-homeproxy-zh-cn"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-passwall"
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-passwall-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -samba4-server"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -samba4-libs"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-app-samba4"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES -luci-i18n-samba4-zh-cn"
 # VPN
 #CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-tailscale luci-i18n-tailscale-zh-cn"
 # 分区扩容 by sirpdboy 
